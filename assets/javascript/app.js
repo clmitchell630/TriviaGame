07f74c0acc48f09e07424b8f2146e98d174ee978
cmitchell <clmitchell630@gmail.com>
2019-07-28 00:01:08 -0500

    JS set up page change to result page and adding result counters

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,60 +6,68 @@ $(function () {
     //array to hold all of the questions and answers
     var trivia = [
         {//question 1
-            q: "Which bear is best?",
-            a: ["Black bear",
-                "Grizzly bear",
-                "Brown bear",
-                "Panda bear"]
+            q: "The Office: According to Jim(dressed as Dwight), which bear is best?",
+            a: ["A. Grizzly bear",
+                "B. Polar bear", 
+                "C. Black bear",
+                "D. All of the above"],
+            c: "2"
         },
-        {//question 1
-            q: "",
-            a: ["",
-                "",
-                "",
-                ""]
+        {//question 2
+            q: "Family Guy: ",
+            a: ["A. ",
+                "B. ",
+                "C. ",
+                "D. "],
+            c: "1"
         },
-        {//question 1
-            q: "",
-            a: ["",
-                "",
-                "",
-                ""]
+        {//question 3
+            q: "Ed, Edd, & Eddy: What is the name of Jonny's best friend?",
+            a: ["A. Plank",
+                "B. Rolf",
+                "C. Jimmy",
+                "D. None of the above"],
+            c: "0"
         },
-        {//question 1
-            q: "",
-            a: ["",
-                "",
-                "",
-                ""]
+        {//question 4
+            q: "The Big Bang Theory: ",
+            a: ["A. ",
+                "B. ",
+                "C. ",
+                "D. "],
+            c: "4"
         },
-        {//question 1
-            q: "",
-            a: ["",
-                "",
-                "",
-                ""]
+        {//question 5
+            q: "Bobs Burgers: ",
+            a: ["A. ",
+                "B. ",
+                "C. ",
+                "D. "],
+            c: "1"
         },
-        {//question 1
-            q: "",
-            a: ["",
-                "",
-                "",
-                ""]
+        {//question 6
+            q: "Seinfield: ",
+            a: ["A. ",
+                "B. ",
+                "C. ",
+                "D. "],
+            c: "4"
         },
-        {//question 1
-            q: "",
-            a: ["",
-                "",
-                "",
-                ""]
+        {//question 7
+            q: "Friends: ",
+            a: ["A. ",
+                "B. ",
+                "C. ",
+                "D. "],
+            c: "0"
         },
-        {//question 1
-            q: "",
-            a: ["",
-                "",
-                "",
-                ""]
+        {//question 8
+            q: "Spongebob Squarepants: Which of these items does Patrick believe are instruments?",
+            a: ["A. Mayonnaise",
+                "B. Horseradish",
+                "C. Both A and B",
+                "D. All of the above"],
+            c: "4"
         },
     ];
 
@@ -67,7 +75,7 @@ $(function () {
     var correct = 0;
     var incorrect = 0;
     var unanswered = 0;
-    var gameStart = false;
+    var questionNum = 0;
 
     //------Initialization------
     $("#game").attr("style", "display: none !important;");
@@ -78,26 +86,34 @@ $(function () {
     $("#start").on("click", function () {
         $("#start").attr("style", "display: none !important;");
         $("#game").attr("style", "display: block !important;");
-        gameStart = true;
     });
-    //initialization after start button is pressed
 
     //------Game------
     //reveal 1st question - last question
-    
-   
-    newQuestion(0);
-    
-    function newQuestion(i){
-        $("#question").text(trivia[i].q);
+    newQuestion(questionNum);
 
-        for(var j = 0; j <trivia[i].a.length; j++){
-            var newDiv = $("<div>");
-            newDiv.addClass("col-md-6 offset-md-3 option");
-            newDiv.text(trivia[i].a[j]);
-            $("#answers").append(newDiv);
+    $(".option").on("click", function(e){
+        //console.log($(this));
+        if($(this).attr("data-name") === trivia[questionNum].c){
+            //console.log("i was clicked");
+            $("#game").attr("style", "display: none !important;");
+            $("#correct").attr("style", "display: block !important;");
+            correct ++;
+            $("#correct").text("Correct: " + correct);
         }
-    }
+        else{
+            //console.log("i was not clicked and something is broken");
+            //console.log(trivia[0].a[trivia[0].c]);
+            $("#game").attr("style", "display: none !important;");
+            $("#results").attr("style", "display: block !important;");
+            incorrect ++;
+            $("#incorrect").text("Incorrect: " + incorrect);
+        }
+        
+            
+    });
+    
+    
     //begin timer from 20 seconds
 
     //.on("click" >> correct answer)
@@ -111,4 +127,18 @@ $(function () {
     //------End game------
     //overall results will appear
     //option to restart the quiz
+
+    //------Functions------
+    
+    function newQuestion(i){
+        $("#question").text(trivia[i].q);
+
+        for(var j = 0; j <trivia[i].a.length; j++){
+            var newDiv = $("<div>");
+            newDiv.addClass("col-md-6 offset-md-3 option");
+            newDiv.attr("data-name", j);
+            newDiv.text(trivia[i].a[j]);
+            $("#answers").append(newDiv);
+        }
+    }
 });
