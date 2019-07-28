85052dc7389291fb2c3711119cccdf7a5480cbb4
cmitchell <clmitchell630@gmail.com>
2019-07-27 19:49:32 -0500

    JS couple of variables defined, pseudo written

---
+++ assets/javascript/app.js
@@ -0,0 +1,91 @@
+console.log(" (\\  (\\\n (^___^)\nc(_(\")(\")\n  Hello!");
+console.log("This RupeeCollector!!");
+
+$(function(){
+    //------Variables------
+        //array to hold all of the questions and answers
+    var trivia = [
+        {//question 1
+            q:"",
+            a1:"",
+            a2:"",
+            a3:"",
+            a4:"",
+        },
+        {//question 2
+            q:"",
+            a1:"",
+            a2:"",
+            a3:"",
+            a4:"",
+        },
+        {//question 3
+            q:"",
+            a1:"",
+            a2:"",
+            a3:"",
+            a4:"",
+        },
+        {//question 4
+            q:"",
+            a1:"",
+            a2:"",
+            a3:"",
+            a4:"",
+        },
+        {//question 5
+            q:"",
+            a1:"",
+            a2:"",
+            a3:"",
+            a4:"",
+        },
+        {//question 6
+            q:"",
+            a1:"",
+            a2:"",
+            a3:"",
+            a4:"",
+        },
+        {//question 7
+            q:"",
+            a1:"",
+            a2:"",
+            a3:"",
+            a4:"",
+        },
+        {//question 8
+            q:"",
+            a1:"",
+            a2:"",
+            a3:"",
+            a4:"",
+        }
+    ];
+
+        //other variables
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+
+    //------Initialization------
+        //start button .on("click" >> initialize)
+
+        //initialization after start button is pressed
+
+    //------Game------
+        //reveal 1st question - last question
+
+        //begin timer from 20 seconds
+
+        /*game logic:
+            if seconds <= 0 then unanswered++, 
+            if question correct then correct ++, 
+            if question incorrect then incorrect ++
+        */
+    //------Results------
+        //new screen will appear displaying your results
+    //------End game------
+        //overall results will appear
+        //option to restart the quiz
+});
