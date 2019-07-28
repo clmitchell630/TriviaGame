140962751c102b1e8c4d743956d8907e7daa2421
cmitchell <clmitchell630@gmail.com>
2019-07-27 21:09:17 -0500

    JS new question function written

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,99 +1,114 @@
 console.log(" (\\  (\\\n (^___^)\nc(_(\")(\")\n  Hello!");
 console.log("This is Trivia Game!!");
 
-$(function(){
+$(function () {
     //------Variables------
-        //array to hold all of the questions and answers
+    //array to hold all of the questions and answers
     var trivia = [
         {//question 1
-            q:"",
-            a1:"",
-            a2:"",
-            a3:"",
-            a4:"",
+            q: "Which bear is best?",
+            a: ["Black bear",
+                "Grizzly bear",
+                "Brown bear",
+                "Panda bear"]
         },
-        {//question 2
-            q:"",
-            a1:"",
-            a2:"",
-            a3:"",
-            a4:"",
+        {//question 1
+            q: "",
+            a: ["",
+                "",
+                "",
+                ""]
         },
-        {//question 3
-            q:"",
-            a1:"",
-            a2:"",
-            a3:"",
-            a4:"",
+        {//question 1
+            q: "",
+            a: ["",
+                "",
+                "",
+                ""]
         },
-        {//question 4
-            q:"",
-            a1:"",
-            a2:"",
-            a3:"",
-            a4:"",
+        {//question 1
+            q: "",
+            a: ["",
+                "",
+                "",
+                ""]
         },
-        {//question 5
-            q:"",
-            a1:"",
-            a2:"",
-            a3:"",
-            a4:"",
+        {//question 1
+            q: "",
+            a: ["",
+                "",
+                "",
+                ""]
         },
-        {//question 6
-            q:"",
-            a1:"",
-            a2:"",
-            a3:"",
-            a4:"",
+        {//question 1
+            q: "",
+            a: ["",
+                "",
+                "",
+                ""]
         },
-        {//question 7
-            q:"",
-            a1:"",
-            a2:"",
-            a3:"",
-            a4:"",
+        {//question 1
+            q: "",
+            a: ["",
+                "",
+                "",
+                ""]
         },
-        {//question 8
-            q:"",
-            a1:"",
-            a2:"",
-            a3:"",
-            a4:"",
-        }
+        {//question 1
+            q: "",
+            a: ["",
+                "",
+                "",
+                ""]
+        },
     ];
 
-        //other variables
+    //other variables
     var correct = 0;
     var incorrect = 0;
     var unanswered = 0;
+    var gameStart = false;
 
     //------Initialization------
     $("#game").attr("style", "display: none !important;");
     $("#results").attr("style", "display: none !important;");
     $("#gameEnd").attr("style", "display: none !important;");
 
-        //start button .on("click" >> initialize)
-    $("#start").on("click", function(){
+    //start button .on("click" >> initialize)
+    $("#start").on("click", function () {
         $("#start").attr("style", "display: none !important;");
         $("#game").attr("style", "display: block !important;");
+        gameStart = true;
     });
-        //initialization after start button is pressed
+    //initialization after start button is pressed
 
     //------Game------
-        //reveal 1st question - last question
+    //reveal 1st question - last question
+    
+   
+    newQuestion(0);
+    
+    function newQuestion(i){
+        $("#question").text(trivia[i].q);
 
-        //begin timer from 20 seconds
+        for(var j = 0; j <trivia[i].a.length; j++){
+            var newDiv = $("<div>");
+            newDiv.addClass("col-md-6 offset-md-3 option");
+            newDiv.text(trivia[i].a[j]);
+            $("#answers").append(newDiv);
+        }
+    }
+    //begin timer from 20 seconds
 
-        //.on("click" >> correct answer)
-        /*game logic:
-            if seconds <= 0 then unanswered++, 
-            if question correct then correct ++, 
-            if question incorrect then incorrect ++
-        */
+    //.on("click" >> correct answer)
+    /*game logic:
+        if seconds <= 0 then unanswered++, 
+        if question correct then correct ++, 
+        if question incorrect then incorrect ++
+    */
     //------Results------
-        //new screen will appear displaying your results
+    //new screen will appear displaying your results
     //------End game------
-        //overall results will appear
-        //option to restart the quiz
+    //overall results will appear
+    //option to restart the quiz
 });
